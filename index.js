84e159f56c7bce2c16714bc401378982cfd9ec1b
Richell Bonfield <richell.bonfield12@gmail.com>
2020-01-08 11:56:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,11 @@
 document.addEventListener("DOMContentLoaded", function() {
   console.log("The DOM has loaded");
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+  console.log("The DOM has loaded");
+});
+ 
+console.log(
+  "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+);
